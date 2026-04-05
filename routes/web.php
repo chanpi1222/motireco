89b28b0574356f58eb574658e110fc65b0789633
ville4649 <chanpi1222@gmail.comssgit config --global user.email chanpi1222@gmail.com>
2026-04-05 11:57:36 +0900

    Change root route to login

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,7 @@ use App\Http\Controllers\HabitAdviceController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('/login');
 });
 
 
